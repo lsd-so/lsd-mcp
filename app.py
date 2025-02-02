6496e448632adf470e2044c0dc01cbd196b4984c
Yev Barkalov <yevbar1729@gmail.com>
2025-02-02 01:11:47 -0500

    only share docs once + add new prompt

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ conn = psycopg2.connect(
     port="5432",
 )
 
+shared_docs = False
+
 @mcp.tool()
 def run_lsd(lsd_sql_code: str) -> List[List[str]]:
     """Runs LSD SQL using user credentials in .env"""
@@ -37,11 +39,29 @@ def fetch_lsd_docs() -> List[Dict[str, str]]:
         return [{"URL": r[0], "MARKDOWN": r[1]} for r in rows]
 
 @mcp.prompt()
-async def write_lsd_sql(objective: str) -> str:
-    # Programmatically inserting docs to context
-    with conn.cursor() as curs:
-        curs.execute("SCAN https://lsd.so/docs")
-        rows = curs.fetchall()
-        lsd_docs = [{"URL": r[0], "MARKDOWN": r[1]} for r in rows]
+def write_lsd_sql(objective: str) -> str:
+    global shared_docs
+
+    if not shared_docs:
+        # Programmatically inserting docs to context
+        with conn.cursor() as curs:
+            curs.execute("SCAN https://lsd.so/docs")
+            rows = curs.fetchall()
+            lsd_docs = [{"URL": r[0], "MARKDOWN": r[1]} for r in rows]
+        shared_docs = True
 
     return f"""Here is documentation for a custom SQL language called LSD in a JSON list of objects where one has a MARKDOWN property with the markdown content of the page and a URL property with the URL of the page it belongs to. {lsd_docs} You may run LSD SQL along the way to obtain HTML or MARKDOWN in order to answer user inquiries. Using the keywords, {objective}"""
+
+@mcp.prompt()
+async def write_and_run_lsd_sql(objective: str) -> str:
+    global shared_docs
+
+    if not shared_docs:
+        # Programmatically inserting docs to context
+        with conn.cursor() as curs:
+            curs.execute("SCAN https://lsd.so/docs")
+            rows = curs.fetchall()
+            lsd_docs = [{"URL": r[0], "MARKDOWN": r[1]} for r in rows]
+        shared_docs = True
+
+    return f"""Here is documentation for a custom SQL language called LSD in a JSON list of objects where one has a MARKDOWN property with the markdown content of the page and a URL property with the URL of the page it belongs to. {lsd_docs} You may run LSD SQL along the way to obtain HTML or MARKDOWN in order to answer user inquiries. Using the keywords, {objective}. When done run the LSD SQL statement and present the results to the user"""
